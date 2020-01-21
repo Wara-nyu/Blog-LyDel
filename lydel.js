9c381975dcff839596bcc02bf0cd7c6dbbbcb8ec
Wara-nyu <w.bounkeua@gmail.com>
2020-01-21 16:36:24 +0100

    ajout de fichiers html

--- lydel.js
+++ lydel.js
@@ -1,7 +1,18 @@
 const http = require('http');
 const url = require('url');
 
-http.createServeur(function (request, respond) {
+http.createServer(function (request, respond) {
+  var pathname = url.parse(request.url).pathname;
+  if (pathname == "/home") {
+      fs.readFile('home.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });  
+  } else {
+        respond.writeHead(404)
+        respond.end("passe ton chemin, bro")
+      }
 
 
-}.listen(8080);
+}).listen(8080);
