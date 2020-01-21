4fc82178c84cb026b1ed896fa82e2113539eeb5d
Wara-nyu <w.bounkeua@gmail.com>
2020-01-22 00:10:48 +0100

    ajout des differents lien et francisation des *.html

--- lydel.js
+++ lydel.js
@@ -4,16 +4,44 @@ const fs = require('fs');
 
 http.createServer(function (request, respond) {
   var pathname = url.parse(request.url).pathname;
-  if (pathname == "/homepage") {
-      fs.readFile('homePage.html', function(err, data) {
+  if (pathname == "/accueil") {
+      fs.readFile('accueil.html', function(err, data) {
         respond.writeHead(200, {'Content-Type': 'text/html'});
         respond.write(data);
         respond.end();
-      });  
+      });
+
+  } else if (pathname == "/parametres")  {
+      fs.readFile('parametres.html', function(err, data) {
+        respond.writeHead(200, {'Content-Type': 'text/html'});
+        respond.write(data);
+        respond.end();
+      });
+
+  } else if (pathname == "/identification")  {
+      fs.readFile('identification.html', function(err, data) {
+        respond.writeHead(200, {'Content-Type': 'text/html'});
+        respond.write(data);
+        respond.end();
+      });
+
+  } else if (pathname == "/nouveau_compte")  {
+      fs.readFile('nouveauCompte.html', function(err, data) {
+        respond.writeHead(200, {'Content-Type': 'text/html'});
+        respond.write(data);
+        respond.end();
+      });
+
+  } else if (pathname == "/nouvel_article")  {
+      fs.readFile('nouvelArticle.html', function(err, data) {
+        respond.writeHead(200, {'Content-Type': 'text/html'});
+        respond.write(data);
+        respond.end();
+      });
+
   } else {
         respond.writeHead(404)
         respond.end("passe ton chemin, bro")
       }
-
 
 }).listen(8080);
